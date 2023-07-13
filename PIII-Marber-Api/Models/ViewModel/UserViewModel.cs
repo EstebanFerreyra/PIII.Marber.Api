2758c465b635f6640346c5d7308cb8490c24da4c
ninacnel <nerinacoronel00@gmail.com>
2023-07-13 15:55:05 -0300

    Fix UserViewModel

--- PIII-Marber-Api/Models/ViewModel/UserViewModel.cs
+++ PIII-Marber-Api/Models/ViewModel/UserViewModel.cs
@@ -8,7 +8,6 @@ namespace Models.ViewModel
 {
     public class UserViewModel
     {
-        public int Id { get; set; }
         public int IdRole { get; set; }
         public string UserName { get; set; } = string.Empty;
         public string UserEmail { get; set; } = string.Empty;
